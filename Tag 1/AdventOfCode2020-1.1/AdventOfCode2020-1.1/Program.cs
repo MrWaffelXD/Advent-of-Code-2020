8a0564b8ca86cd159ed014a92524e015d3d1989f
Jonas Heinze <jonas.heinze@truetzschler.de>
2020-12-03 15:31:17 +0100

    init, day 1, day 2

---
+++ Tag 1/AdventOfCode2020-1.1/AdventOfCode2020-1.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AdventOfCode2020_1._1
+{
+    class Program
+    {
+        static void task1()
+        {
+            string[] numbers = System.IO.File.ReadAllLines("..\\..\\..\\input.txt");
+            int arrl = numbers.Length;
+
+            int curr = 0;
+            foreach (string sn in numbers)
+            {
+                int n = Convert.ToInt32(sn);
+                curr = n;
+                for (int i = 0; i < arrl; i++)
+                {
+                    if (curr + Convert.ToInt32(numbers[i]) == 2020)
+                    {
+                        Console.WriteLine("{0} + {1} = 2020", curr, Convert.ToInt32(numbers[i]));
+                        Console.WriteLine("{0} * {1} = {2}", curr, Convert.ToInt32(numbers[i]), curr * Convert.ToInt32(numbers[i]));
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
+            Console.WriteLine("End of program!");
+        }
+        static void task2()
+        {
+            string[] numbers = System.IO.File.ReadAllLines("..\\..\\..\\input.txt");
+            int arrl = numbers.Length;
+
+            for(int fi = 0; fi<arrl; fi++)
+            {
+                int currf = Convert.ToInt32(numbers[fi]);//get first number
+                for (int si = 0; si<arrl; si++)
+                {
+                    int currs = Convert.ToInt32(numbers[si]);//get first number
+                    for(int ti = 0; ti<arrl; ti++)
+                    {
+                        int currt = Convert.ToInt32(numbers[ti]);
+                        if(currf + currs + currt == 2020)
+                        {
+                            Console.WriteLine("{0} + {1} + {2} = 2020", currf, currs, currt);
+                            Console.WriteLine("{0} * {1} * {2} = {3}", currf, currs, currt, currf * currs * currt);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("End of program!");
+        }
+        static void Main(string[] args)
+        {
+            task2();
+        }
+    }
+}
---
+++ Tag 2/AdventOfCode2020-2.1/AdventOfCode2020-2.1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020_2._1
+{
+    //count valid passwords from policy
+    class Policy
+    {
+        //1-13 r: gqdrspndrpsrjfjx
+        public int max { get; set; }
+        public int min { get; set; }
+        public char character { get; set; }
+        public string password { get; set; }
+    }
+    class Program
+    {
+        public static int task1()
+        {
+            string[] passwords = System.IO.File.ReadAllLines("..\\..\\..\\passwords.txt");
+
+            char[] spearator = { ' ' };
+            int counter = 0;
+            foreach (string p in passwords)
+            {
+                string[] strlist = p.Split(spearator);
+                /*
+                1-13
+                r:
+                gqdrspndrpsrjfjx
+                */
+                Policy pw = new Policy();
+
+                string[] qty = strlist[0].Split("-");
+
+                pw.min = Convert.ToInt32(qty[0]);
+                pw.max = Convert.ToInt32(qty[1]);
+                pw.character = strlist[1][0];
+                pw.password = strlist[2];
+
+                int count = pw.password.Count(f => f == pw.character);//linq - count occurence of char in str
+                if (count >= pw.min && count <= pw.max)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public static int task2()
+        {
+            string[] passwords = System.IO.File.ReadAllLines("..\\..\\..\\passwords.txt");
+
+            char[] spearator = { ' ' };
+            int counter = 0;
+            foreach (string p in passwords)
+            {
+                string[] strlist = p.Split(spearator);
+
+                Policy pw = new Policy();
+
+                string[] qty = strlist[0].Split("-");
+
+                pw.min = Convert.ToInt32(qty[0]);
+                pw.max = Convert.ToInt32(qty[1]);
+                pw.character = strlist[1][0];
+                pw.password = strlist[2];
+
+                char first = pw.password[pw.min - 1];
+                char second = pw.password[pw.max - 1];
+
+                if(first != second && first == pw.character || first != second && second == pw.character)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are {0} passwords matching the policy!", task1());
+            Console.WriteLine("There are {0} passwords matching the new policy!", task2());
+        }
+    }
+}
