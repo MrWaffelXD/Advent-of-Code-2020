b193841f930e5f46b8840e0633ad687191fa633a
MrWaffelXD <jonas@jonas-heinze.de>
2020-12-04 18:56:20 +0100

    Day 3 task 2

--- Tag 3/AdventOfCode2020-3.1/AdventOfCode2020-3.1/Program.cs
+++ Tag 3/AdventOfCode2020-3.1/AdventOfCode2020-3.1/Program.cs
@@ -4,7 +4,8 @@ namespace AdventOfCode2020_3._1
 {
     class Program
     {
-        static int task1()
+        //slide down a hill and counting trees by map
+        static int task1(int xMove = 3, int yMove = 1)
         {
             string[] map = System.IO.File.ReadAllLines("input.txt");//last char 30 (-31)
 
@@ -17,9 +18,9 @@ namespace AdventOfCode2020_3._1
                     trees++;
                 }
 
-                x = x + 3;
+                x = x + xMove;
 
-                y++;
+                y = y + yMove;
                 if (x > 30)
                 {
                     x = x - 31;
@@ -29,7 +30,15 @@ namespace AdventOfCode2020_3._1
         }
         static void Main(string[] args)
         {
+            //task1
             Console.WriteLine("You hit {0} trees.", task1());
+            //task2.1
+            Int64 total = task1(1, 1);//somehow get out of range values when multiplying in one line
+            total = total * task1(3, 1);
+            total = total * task1(5, 1);
+            total = total * task1(7, 1);
+            total = total * task1(1, 2);
+            Console.WriteLine("You would hit a total of {0} trees on the slopes.", total);
         }
     }
 }
