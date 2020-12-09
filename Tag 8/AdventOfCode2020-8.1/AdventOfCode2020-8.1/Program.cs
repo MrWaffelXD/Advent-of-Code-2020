7217827a6a6646d6e82e90e5a8e88032cfc565a8
Jonas Heinze <jonas.heinze@truetzschler.de>
2020-12-09 16:26:04 +0100

    unfinished day 8 task 2

--- Tag 8/AdventOfCode2020-8.1/AdventOfCode2020-8.1/Program.cs
+++ Tag 8/AdventOfCode2020-8.1/AdventOfCode2020-8.1/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode2020_8._1
         public static int performJump(int v, int old)
         {
             int value = old + v;
-            Console.WriteLine("Jump to {0}", value);
+            Console.WriteLine("Jump to {0}", value - 1);
             return value -1;
         }
         public static int changeAcc(int v, int accu)
@@ -18,7 +18,7 @@ namespace AdventOfCode2020_8._1
             Console.WriteLine("New Accu: {0}", value);
             return value;
         }
-        static void Main(string[] args)
+        static void task1()
         {
             string instructions = System.IO.File.ReadAllText("input.txt");
             string[] stepsArr = instructions.Split("\n");
@@ -30,7 +30,7 @@ namespace AdventOfCode2020_8._1
             foreach (var s in stepsArr)
             {
                 var temp = s;
-                temp = temp.Replace("\r","");
+                temp = temp.Replace("\r", "");
                 steps.Add(temp.Split(" "));
                 Console.WriteLine(s);
             }
@@ -62,7 +62,81 @@ namespace AdventOfCode2020_8._1
 
             }
             Console.WriteLine("Done");
-            Console.WriteLine("Accumulator: {0}" + accu);
+            Console.WriteLine("Accumulator: {0}", accu);
+        }
+        static void task2()
+        {
+            string instructions = System.IO.File.ReadAllText("input.txt");
+            string[] stepsArr = instructions.Split("\n");
+
+            List<string[]> steps = new List<string[]>();
+            List<int> doneSteps = new List<int>();
+            List<int> switchedSteps = new List<int>();
+
+            Console.WriteLine("Instruction set:");
+            foreach (var st in stepsArr)
+            {
+                var temp = st;
+                temp = temp.Replace("\r", "");
+                steps.Add(temp.Split(" "));
+                Console.WriteLine(st);
+            }
+            int s = 0, accu = 0;
+            bool ended = false;
+            while (!ended)
+            {
+                bool switched = false;//reset only on new try
+                if (!doneSteps.Contains(s))
+                {
+                    doneSteps.Add(s);
+                    switch (steps[s][0])
+                    {
+                        case "acc": //change value
+                            accu = changeAcc(Convert.ToInt32(steps[s][1]), accu);
+                            s++;
+                            break;
+                        case "jmp":
+                            if (!switchedSteps.Contains(s) && !switched)
+                            {
+                                Console.WriteLine("Changed jmp to nop");
+                                switched = true;
+                                switchedSteps.Add(s);
+                                s++;
+                                break;
+                            }
+                            else
+                            {
+                                s = performJump(Convert.ToInt32(steps[s][1]), s);
+                                s++;
+                                break;
+                            }
+                        case "nop":
+                            if (!switchedSteps.Contains(s) && !switched)
+                            {
+                                Console.WriteLine("Changed nop to jmp");
+                                s = performJump(Convert.ToInt32(steps[s][1]), s);
+                                s++;
+                                break;
+                            }
+                            else
+                            {
+                                s++;
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    //ended = false;
+                }
+
+            }
+            Console.WriteLine("Done");
+            Console.WriteLine("Accumulator: {0}", accu);
+        }
+        static void Main(string[] args)
+        {
+            task2();
         }
     }
 }
