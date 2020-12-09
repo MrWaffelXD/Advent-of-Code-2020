5dbb1dcae018462ff6285ccbcdbb93a0d798dc08
MrWaffelXD <jonas@jonas-heinze.de>
2020-12-09 18:59:54 +0100

    finished day 8 task 2

--- Tag 8/AdventOfCode2020-8.1/AdventOfCode2020-8.1/Program.cs
+++ Tag 8/AdventOfCode2020-8.1/AdventOfCode2020-8.1/Program.cs
@@ -70,7 +70,6 @@ namespace AdventOfCode2020_8._1
             string[] stepsArr = instructions.Split("\n");
 
             List<string[]> steps = new List<string[]>();
-            List<int> doneSteps = new List<int>();
             List<int> switchedSteps = new List<int>();
 
             Console.WriteLine("Instruction set:");
@@ -82,56 +81,71 @@ namespace AdventOfCode2020_8._1
                 Console.WriteLine(st);
             }
             int s = 0, accu = 0;
-            bool ended = false;
-            while (!ended)
+            bool finished = false;
+            while (!finished)
             {
-                bool switched = false;//reset only on new try
-                if (!doneSteps.Contains(s))
+                accu = 0;
+                s = 0;
+                List<int> doneSteps = new List<int>();
+                bool error = false, swapped = false; ;
+                while (!error && !finished)
                 {
-                    doneSteps.Add(s);
-                    switch (steps[s][0])
+                    if (!doneSteps.Contains(s) && s < steps.Count && finished == false)
                     {
-                        case "acc": //change value
-                            accu = changeAcc(Convert.ToInt32(steps[s][1]), accu);
-                            s++;
-                            break;
-                        case "jmp":
-                            if (!switchedSteps.Contains(s) && !switched)
-                            {
-                                Console.WriteLine("Changed jmp to nop");
-                                switched = true;
-                                switchedSteps.Add(s);
+                        doneSteps.Add(s);
+                        switch (steps[s][0])
+                        {
+                            case "acc": //change value
+                                accu = changeAcc(Convert.ToInt32(steps[s][1]), accu);
                                 s++;
                                 break;
-                            }
-                            else
-                            {
-                                s = performJump(Convert.ToInt32(steps[s][1]), s);
-                                s++;
-                                break;
-                            }
-                        case "nop":
-                            if (!switchedSteps.Contains(s) && !switched)
-                            {
-                                Console.WriteLine("Changed nop to jmp");
-                                s = performJump(Convert.ToInt32(steps[s][1]), s);
-                                s++;
-                                break;
-                            }
-                            else
-                            {
-                                s++;
-                                break;
-                            }
-                    }
-                }
-                else
-                {
-                    //ended = false;
-                }
+                            case "jmp":
+                                if (!switchedSteps.Contains(s) && !swapped)//not previously swapped
+                                {
+                                    Console.WriteLine("Swapped jmp to nop at line: {0}", s);
+                                    switchedSteps.Add(s);
+                                    swapped = true;
+                                    s++;
+                                    break;
+                                }
+                                else
+                                {    
+                                    s = performJump(Convert.ToInt32(steps[s][1]), s);
+                                    s++;
+                                    break;
+                                }
 
+                            case "nop":
+                                if (!switchedSteps.Contains(s) && !swapped)
+                                {
+                                    Console.WriteLine("Swapped nop to jmp at line: {0}", s);
+                                    switchedSteps.Add(s);
+                                    swapped = true;
+                                    s = performJump(Convert.ToInt32(steps[s][1]), s);
+                                    s++;
+                                    break;
+                                }
+                                else
+                                {
+                                    s++;
+                                    break;
+                                }
+
+                        }
+                    }
+                    else if(s == steps.Count)
+                    {
+                        finished = true;
+                    }
+                    else//performed action previously
+                    {
+                        error = true;
+                        break;
+                    }
+
+                }
             }
-            Console.WriteLine("Done");
+            Console.WriteLine("\n----------\n\nDone");
             Console.WriteLine("Accumulator: {0}", accu);
         }
         static void Main(string[] args)
